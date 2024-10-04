6af2a28d82e89d8e1361dae8848f7f34014a942c
Abrish <abrish4ever2015@gmail.com>
2024-10-04 13:32:35 +0300

    Proj: Add Components to the Application

---
+++ src/components/WeatherApp.jsx
@@ -0,0 +1,115 @@
+import '../styles/WeatherApp.css'
+import { FaMapMarker } from "react-icons/fa";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import loadingGif from '../assets/images/loading.gif';
+import { useEffect, useState } from 'react';
+import WeatherDisplay from './WeatherDisplay';
+
+
+
+const WeatherApp = () => {
+    const [data, setData] = useState({});
+    const [location, setLocation] = useState('');
+    const [loading, setLoading] = useState(false)
+
+    const api_key = import.meta.env.VITE_WEATHER_API_KEY;
+
+    useEffect(() => {
+        const fetchDefaultWeather = async () => {
+            setLoading(true)
+            const defaultLocation = "Addis Ababa";
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${defaultLocation}&units=Metric&appid=${api_key}`;
+            const res = await fetch(url);
+            const defaultData = await res.json();
+            setData(defaultData)
+            setLoading(false)
+        }
+        fetchDefaultWeather();
+    }, []);
+
+    const handleInputLocation = (e) => {
+        setLocation(e.target.value)
+    }
+    const search = async () => {
+        if (location.trim() !== '') {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Metric&appid=${api_key}`;
+            const res = await fetch(url);
+            const searchData = await res.json();
+            if (searchData.cod !== 200) {
+                setData({ notFound: true })
+            } else {
+                setData(searchData);
+                setLocation('')
+
+            }
+            setLoading(false)
+        }
+    }
+
+
+    // useEffect to fetch data initially and set interval for real-time updates
+  useEffect(() => {
+    search(); // Initial fetch
+
+    // Update weather every 5 minutes (300,000 milliseconds)
+    const intervalId = setInterval(search, 3000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
+
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            search()
+        }
+    }
+
+    const backgroundImages = {
+        Clear: 'linear-gradient(to right, #f3b07c, #fcd283)',
+        Clouds: 'linear-gradient(to right, #f3b07c, #9f8b91)',
+        Rain: 'linear-gradient(to right, #5bc8fb, #80eaff)',
+        Snow: 'linear-gradient(to right, #aff2ff, #fff)',
+        Haze: 'linear-gradient(to right, #f3b07c, #71eeec)',
+        Mist: 'linear-gradient(to right, #f3b07c, #71eeec)',
+    }
+    const backgroundImage = data.weather ? backgroundImages[data.weather[0].main] : 'linear-gradient(to right, #f3b07c, #fcd283)';
+
+
+    return (
+        <div className="w-full h-[100vh] bg-fixed flex bg-[#FCF5E5] justify-center items-center overflow-hidden">
+            <div className="w-[40rem] h-[60rem] m-8 md:m-0 flex flex-col items-center relative p-8 rounded-[3rem] shadow-[-3rem 3rem 6rem rgba(0,0,0,0.1)]" style={{ backgroundImage }} >
+                <div className="header">
+                    <div className="name">
+                        <h1>Weather Dashboared </h1>
+                    </div>
+                    <div className="search">
+                        <div className="search-top">
+                            <FaMapMarker className="icon " />
+                            <div className="location">{data.name}</div>
+                        </div>
+                        <div className="search-bar">
+                            <input
+                                type="text"
+                                placeholder="Enter location"
+                                value={location}
+                                onChange={handleInputLocation}
+                                onKeyDown={handleKeyDown}
+                            />
+                            <FaMagnifyingGlass className="icon" onClick={search} />
+                        </div>
+                        <button onClick={search} className="bg-blue-500 text-white px-4 py-2 mt-4"> Refresh Weather</button>
+                    </div>
+                </div>
+                {loading ? (<img className="loader" src={loadingGif} alt="loading" />) : data.notFound ? (<div className='not-found'>Not Found</div>) :
+                    (
+                        <WeatherDisplay data={data} />
+                    )
+                }
+
+            </div>
+        </div>
+    );
+}
+
+export default WeatherApp
--- src/components/WeatherDisplay.jsx
+++ src/components/WeatherDisplay.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import '../styles/WeatherDisplay.css'
+import { FaMapMarker } from 'react-icons/fa'
+import { FaDroplet } from 'react-icons/fa6'
+import { FaWind } from 'react-icons/fa6'
+import sunny from '../assets/images/sunny.png';
+import cloudy from '../assets/images/cloudy.png';
+import rainy from '../assets/images/rainy.png';
+import snowy from '../assets/images/snowy.png';
 
-const WeatherDisplay = () => {
-  return (
-    <div>WeatherDisplay</div>
-  )
+
+const WeatherDisplay = ({data}) => {
+
+    const weatherImages = {
+        Clear: sunny,
+        Clouds: cloudy,
+        Rain: rainy,
+        Snow: snowy,
+        Haze: cloudy,
+        Mist: cloudy,
+    }
+    const weatherImage = data.weather ? weatherImages[data.weather[0].main] : null;
+
+    const currentDate = new Date()
+    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const dayOfWeek = daysOfWeek[currentDate.getDay()];
+    const month = months[currentDate.getMonth()];
+    const dayOfMonth = currentDate.getDate();
+    const year = currentDate.getFullYear();
+    const formattedDate = `${dayOfWeek}, ${dayOfMonth} ${month} ${year}`;
+
+    return (
+        <>
+            <div className="weather">
+                <img src={weatherImage} className="w-[300rem]" alt="sunny" />
+                <div className="absolute top-[40%] left-[50%] translate-x-[-50%] text-5xl text-gray-800">
+                    {data.weather ? data.weather[0].main : null}
+                </div>
+                <div className="temp">{data.main ? `${Math.floor(data.main.temp)}°c` : null}</div>
+            </div>
+            <div className="weather-date">
+                <p>{formattedDate}</p>
+            </div>
+            <div className="weather-data">
+                <div className="humidity">
+                    <div className="data-name">Humidity</div>
+                    <FaDroplet className="icon" />
+                    <div className="data">{data.main ? data.main.humidity : null} %</div>
+                </div>
+                <div className="wind">
+                    <div className="data-name">Wind</div>
+                    <FaWind className="icon" />
+                    <div className="data">{data.wind ? data.wind.speed : null}Km/h</div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default WeatherDisplay
